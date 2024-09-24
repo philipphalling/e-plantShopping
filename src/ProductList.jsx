134b556f63929be2e7fa53cc50ae01abef07167a
Philipp Halling <philipp.halling@gmail.com>
2024-09-24 19:26:15 +0000

    corrected rendering

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -334,17 +334,29 @@ function ProductList() {
       </div>
       {!showCart ? (
         <div className="product-grid">
-          {plantsArray.map((data, index) => (
+          {plantsArray.map((category, index) => (
             <div key={index}>
-              <h1>{data.category}</h1>
-              {data.plants.map((c, i) => (
-                <div key={i}>
-                  <img src={c.image} />
-                  <h3>{c.name}</h3>
-                  <h3>{c.description}</h3>
-                  <hr />
-                </div>
-              ))}
+              <h1>
+                <div>{category.category}</div>
+              </h1>
+              <div className="product-list">
+                {category.plants.map((plant, plantIndex) => (
+                  <div className="product-card" key={plantIndex}>
+                    <img
+                      className="product-image"
+                      src={plant.image}
+                      alt={plant.name}
+                    />
+                    <div className="product-title">{plant.name}</div>
+                    {/*Similarly like the above plant.name show other details like description and cost*/}
+                    <button
+                      className="product-button"
+                      onClick={() => handleAddToCart(plant)}>
+                      Add to Cart
+                    </button>
+                  </div>
+                ))}
+              </div>
             </div>
           ))}
         </div>
